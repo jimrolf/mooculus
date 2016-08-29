4fb33ea57194eca35e587874923408cf1bce77a0
Jim Fowler <fowler@math.osu.edu>
2016-08-29 00:39:29 -0400

    Updated interactive to save name

--- name.js
+++ name.js
@@ -4,22 +4,31 @@ define(['jquery', 'tincan'], function($, TinCan) {
     target.width("100%");
     target.height(300);
 
-    target.append( $('<div class="input-group">' +
-		     '  <input type="text" class="form-control">' +
-		     '  <span class="input-group-btn">' +
-		     '    <button class="btn btn-default" type="button">Finish Exam</button>' +
-		     '  </span>' +
-		     '</div>') );
-
-    $('.btn', target).click( function() {
-	var text = $('input', target).val().toLowerCase().trim();
-	text = text.replace(/@.*/, '');
-	
-	if (text.match( /^[a-z]+\.[0-9]+$/ )) {
-	    alert('Student ' + text + '@osu.edu has finished this exam.');
+    target.persistentData(function(event) {
+	if (target.persistentData( 'name.number')) {
+	    target.empty();
+	    target.append( $('<p>' + target.persistentData( 'name.number') + '</p>') );
 	} else {
-	    alert('Your email address should be your @osu.edu email.  It must consist of all or some portion of your last name, a dot, and a number.');
-	    return false;
+	    target.empty();	    
+	    target.append( $('<div class="input-group">' +
+			     '  <input type="text" class="form-control">' +
+			     '  <span class="input-group-btn">' +
+			     '    <button class="btn btn-default" type="button">Finish Exam</button>' +
+			     '  </span>' +
+			     '</div>') );
+	    
+	    $('.btn', target).click( function() {
+		var text = $('input', target).val().toLowerCase().trim();
+		text = text.replace(/@.*/, '');
+		
+		if (text.match( /^[a-z]+\.[0-9]+$/ )) {
+		    target.persistentData( 'name.number', text );
+		    alert('Student ' + text + '@osu.edu has finished this exam.');
+		} else {
+		    alert('Your email address should be your @osu.edu email.  It must consist of all or some portion of your last name, a dot, and a number.');
+		    return false;
+		}
+	    });
 	}
     });
     
@@ -34,7 +43,6 @@ define(['jquery', 'tincan'], function($, TinCan) {
     });
     
     target.click( function() {
-	target.persistentData( 'correct', true );
 	target.trigger( 'ximera:correct' );
     });*/
 });

75eaba3ea4b568503afdd693fe77d5ba1995dadd
Jim Fowler <fowler@math.osu.edu>
2016-08-29 00:28:57 -0400

    thank you page

---
+++ name.js
@@ -0,0 +1,28 @@
+define(['jquery', 'tincan'], function($, TinCan) {
+    var target = this;
+    
+    target.width("100%");
+    target.height(300);
+
+    target.insert( $('<div class="input-group">' +
+		     '  <input type="text" class="form-control">' +
+		     '  <span class="input-group-btn">' +
+		     '    <button class="btn btn-default" type="button">Go!</button>' +
+		     '  </span>' +
+		     '</div>') );
+
+    /*
+    target.trigger( 'ximera:answer-needed' );
+
+    target.persistentData(function(event) {
+	if (target.persistentData( 'correct'))
+	    target.css('background-color','#DFE');
+	else
+	    target.css('background-color','#EEE');	    
+    });
+    
+    target.click( function() {
+	target.persistentData( 'correct', true );
+	target.trigger( 'ximera:correct' );
+    });*/
+});
